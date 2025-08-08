780fa60f5db2cfc1f156ec3faf52a5897d654901
AlmaSotelo <angieandme17@gmail.com>
2025-08-07 23:51:12 -0700

    Problems 1, 2 & 4 solved.

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,33 +13,68 @@ public class Node
     {
         // TODO Start Problem 1
 
+        if (value == Data) // if value is already in Data
+            return;        // do not insert
         if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data) // is the current node the one I'm looking for?
+            return true;
+
+        if (value < Data)
+            if (Left != null)
+                return Left.Contains(value);
+            else
+                return false;
+
+        if (Right != null)
+            return Right.Contains(value);
+        else
+            return false;
+      
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        // If there are no children, then heigh is 1
+        if (Left == null && Right == null)
+            return 1;
+
+        // Get the height of the Left side
+        int leftHeight = 0;
+        if (Left != null)
+            leftHeight = Left.GetHeight();
+
+        // Get the height of the Right side
+        int rightHeight = 0;
+        if (Right != null)
+            rightHeight = Right.GetHeight();
+
+        // Calculate Hight of "this" tree
+        // Height of current tree = 1 (current node) + the bigger side
+        if (leftHeight > rightHeight)
+            return 1 + leftHeight;
+        else
+            return 1 + rightHeight;    
     }
 }
